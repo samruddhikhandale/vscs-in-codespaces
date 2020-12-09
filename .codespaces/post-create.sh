d2d8bc0c1cf944ea176d4345278f0993d583620c
anpaskin <33612256+anpaskin@users.noreply.github.com>
2020-12-09 11:19:47 -0800

    Update post-create.sh

--- .codespaces/post-create.sh
+++ .codespaces/post-create.sh
@@ -1,12 +1,3 @@
-# install dotnet sdk
-curl -SL https://dotnetcli.blob.core.windows.net/dotnet/Sdk/3.1.404/dotnet-sdk-3.1.404-linux-x64.tar.gz --output dotnet.tar.gz
-tar -zxf dotnet.tar.gz -C /home/codespace/.dotnet --skip-old-files
-rm dotnet.tar.gz
-
-echo "deb http://security.debian.org/debian-security jessie/updates main" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update -y
-sudo apt-get install -y --no-install-recommends libssl1.0.0
-
 # add oh-my-bash
 wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O - | sh -C
 
