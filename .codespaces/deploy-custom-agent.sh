4a426123e9e333cf971bcb02fbb78f1774eaa47b
joshspicer <hello@joshspicer.com>
2020-12-16 23:10:40 +0000

    deploy custom agent script

---
+++ .codespaces/deploy-custom-agent.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#ERROR_STR=" See /tmp/deploy-custom-agent.log for info"
+
+function check_err {
+    if [ $? -ne 0 ]; then
+    echo "⚠ Could not $1. Please try this step manually."
+    exit 1
+    fi
+}
+
+if [ $1 != "--no-build" ]; then
+    echo "🚀 Building your AGENT changes..."
+    dotnet build $CASCADE_ROOT/src/VSOnline > /dev/null
+    check_err "build agent"
+
+    pushd $VSCLK_ROOT
+
+    echo "🚀 Building your VSCLK-CORE changes..."
+    dotnet build $VSCLK_ROOT  > /dev/null
+    check_err "build vsclk-core"
+fi
+
+echo "🚀 Generating artifacts..."
+dotnet $CASCADE_ROOT/bin/Debug/VSOnline.DevTool/DevTool.dll generateArtifacts $CASCADE_ROOT | tee /tmp/gen_artifacts.txt
+check_err "generate artifacts"
+
+ARTIFACTS_PATH=`cat /tmp/gen_artifacts.txt | awk '/Artifact located at: /{print $NF}'`
+ARTIFACTS_ID=`echo $ARTIFACTS_PATH |grep -o '[0-9]\+'`
+
+echo "########"
+echo $ARTIFACTS_PATH
+echo $ARTIFACTS_ID
+echo "#####"
+
+echo "🚀 Uploading artifacts..."
+export UseSecretFromAppConfig=1
+pushd ~/CEDev
+dotnet $VSCLK_ROOT/bin/debug/VsoUtil/VsoUtil.dll preparedevcli -c $ARTIFACTS_PATH -v $ARTIFACTS_ID
+check_err "uploading artifacts"
+
+echo "🎊 All done!"
