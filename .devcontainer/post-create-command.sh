3975aa0f460d660e1918bd95e45ee8b11b07418d
Aaron Paskin <aaronpaskin@microsoft.com>
2021-01-11 17:25:33 -0500

    Nuget cred provider

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -2,4 +2,5 @@
 
 # install yarn dependencies in the `workspaceFolder`
 # directory defined by $ADO_REPO_DEFAULT_PATH
+wget -qO- https://aka.ms/install-artifacts-credprovider.sh | bash
 yarn install
