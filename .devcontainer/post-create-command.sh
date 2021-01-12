5051adc09324a0720e1943096b83380716907094
anpaskin <33612256+anpaskin@users.noreply.github.com>
2021-01-12 10:49:14 -0500

    Update post-create-command.sh

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -2,5 +2,4 @@
 
 # install yarn dependencies in the `workspaceFolder`
 # directory defined by $ADO_REPO_DEFAULT_PATH
-wget -qO- https://aka.ms/install-artifacts-credprovider.sh | bash
 yarn install
